e670bbaae4635502124a4966abfc94748eba8452
league <jkfdzljfdljkjkxgljs>
2024-04-27 11:26:33 -0700

    finished one

--- _01_function_writing/_d_happy_pet.py
+++ _01_function_writing/_d_happy_pet.py
@@ -2,9 +2,10 @@
 Write methods to represent the activities that will make the user's pet happy.
 """
 from tkinter import messagebox, simpledialog, Tk
-
+import turtle
 
 if __name__ == '__main__':
+    jack = turtle.Turtle()
     # TODO)
     #   1. Ask the user to enter the type of pet they want (give them a few
     #      choices).
@@ -15,4 +16,27 @@ if __name__ == '__main__':
     #      Each activity should increase (or decrease) the pet's happiness
     #      level by a different amount, depending on the kind of pet they
     #      have. For example, a fish might not enjoy a walk!
+    pet = simpledialog.askstring(title='pet selector',
+                                 prompt='what pet do you want? dog,eagle,shark, mountain lion, panda, human')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     pass
+
+
+
+
+
+
